36406ff40b0187c3bd815034249b8035d8518e1c
Pavlo Trykulich <66869015+ptrykulich@users.noreply.github.com>
2023-11-14 14:25:32 +0200

    fix: change implement of function getRandomInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -604,7 +604,7 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -1, 1 => -1 | 0 | 1
  */
 function getRandomInteger(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 /**
